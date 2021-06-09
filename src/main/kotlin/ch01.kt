45062000ac2c2c44cb289919645b35440de89e60
syn <yuncong.wang@gmail.com>
2021-06-09 22:26:39 +0800

    chapter 01 exercise

---
+++ src/main/kotlin/ch01.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    val a: Int = args[0].toInt()
+    val b: Int = args[1].toInt()
+    val c: Int = args[2].toInt()
+    println("first arg: $a, second arg: $b, third arg: $c")
+
+    if (a == b && a == c) {
+        println("equal")
+    }
+    else {
+        println("not equal")
+    }
+}
