c96cef0ef541ad994ae9d3de8cb1ca1eb38774d1
syn <yuncong.wang@gmail.com>
2021-06-24 15:38:35 +0800

    ex 1.2.6 implementation

--- src/main/kotlin/ch01/s02/ex_01_02_06.kt
+++ src/main/kotlin/ch01/s02/ex_01_02_06.kt
@@ -12,9 +12,17 @@ fun main(args: Array<String>) {
     val t = args[1]
 
     val isCircularRotate  = fun(s: String, t: String): Boolean {return (s.length == t.length) && (s + s).contains(t)}
+    val isCircularRotate2 = { s:String, t: String -> (s.length == t.length) && (s + s).contains(t) }
+    val isCircularRotate3 = ::isCircularRotateFun
 
     println("$s and $t is circular retated string pair: ${isCircularRotate(s, t)}")
+    println("$s and $t is circular retated string pair: ${isCircularRotate2(s, t)}")
+    println("$s and $t is circular retated string pair: ${isCircularRotate3(s, t)}")
 
+}
+
+fun isCircularRotateFun(s: String, t: String): Boolean {
+    return (s.length == t.length) && (s + s).contains(t)
 }
 
 
