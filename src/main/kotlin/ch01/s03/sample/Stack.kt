1ef6ebe948c7e45a1a9394ad05697b3aa54170d8
syn <yuncong.wang@gmail.com>
2021-06-26 17:13:21 +0800

    add stack sample code

---
+++ src/main/kotlin/ch01/s03/sample/Stack.kt
@@ -0,0 +1,50 @@
+package ch01.s03.sample
+
+import edu.princeton.cs.algs4.StdIn
+import edu.princeton.cs.algs4.StdOut
+
+class Stack<T>{
+    class Node<T>(val item: T, var next: Node<T>? = null)
+
+    var first: Node<T>? = null
+    var n: Int = 0
+
+    public fun isEmpty(): Boolean {
+        return first == null
+    }
+
+    public fun size(): Int {
+        return n
+    }
+
+    val psize: Int
+        get() = this.n
+
+    public fun push(item: T): Unit {
+        val oldFirst = first
+        first = Node(item, oldFirst)
+        n++
+    }
+
+    public fun pop(): T {
+        val item = first!!.item
+        first = first!!.next
+        n--
+        return item
+    }
+}
+
+fun main(args: Array<String>) {
+    val s = Stack<String>()
+
+    while(!StdIn.isEmpty()) {
+        val item = StdIn.readString()
+        if (!item.equals("-")) {
+            s.push(item)
+        } else if (!s.isEmpty()) {
+            val str: String = s.pop()
+            StdOut.print(str + " ")
+        }
+    }
+    StdOut.println("( " + s.size() + " left on stack)")
+}
